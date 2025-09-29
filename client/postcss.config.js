56a8dcac5008a497841e5d96d790b4ab5300d221
Edoghotu Gideon Azibaobuom <118543750+gideonEdoghotu@users.noreply.github.com>
2025-09-29 16:56:39 +0100

    updated

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -5,11 +5,19 @@
 //   },
 // }
 
+// export default {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//     'postcss-nesting': {},
+//     'postcss-modules': {},
+//     autoprefixer: {},
+//   },
+// }
+
 export default {
   plugins: {
-    '@tailwindcss/postcss': {},
-    'postcss-nesting': {},
-    'postcss-modules': {},
+    "@tailwindcss/postcss": {},   // ✅ use the new plugin package
     autoprefixer: {},
   },
 }
+
